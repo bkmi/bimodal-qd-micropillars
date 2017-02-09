ee20fad02f5c67ffa6152313f5d49314df224849
Ben M <benmiller1315@gmail.com>
2017-02-09 15:28:42 +0100

    Added support for arbitrary rotations in m_rot_rhs

--- functions/rot/m_sys_rhs_RWFold.m
+++ functions/rot/m_sys_rhs_RWFold.m
@@ -48,7 +48,8 @@ end
 %         length(p),...          % select the parameter which is perturbed, omega
 %         hbif);                 % this is the size of the deviation.
 
-dfom = cell(numel(rho));
+numRho = numel(rho);
+dfom = cell(numel(rho),1);
 % Create a dir deriv for each rho parameter we have. Organize them
 % into a cell, so they can multiple by the parameter later on.
 for i = 1:numel(rho)
@@ -56,7 +57,7 @@ for i = 1:numel(rho)
     @(p)orig_rhs(x0,p),... % function to provide, devations are in par
     p,...                  % parameter set
     1,...                  % deviation = 1 * hbif
-    length(p)-1,...        % select the parameter which is perturbed, omega
+    length(p)-numRho+i,... % select the parameter which is perturbed, omega
     hbif);                 % this is the size of the deviation.
 
     dfom{i} = TEMPdfom;
@@ -67,107 +68,6 @@ for i = 1:numel(rho)
     y1 = y1 + dfom{i}*rho(i);
 end
 y=[y0;y1];
-        
-
-%% Previous attempts, just for reference...
-
-%{
-
-switch numel(rho)
-    case 1
-        %% How the system worked with only a single omega
-                x0=x(1:dim,:); % the actual guess/soln vector
-        v=x(dim+1:end,:); % the nullvector
-        y0=orig_rhs(x0,p);
-        dfx=@(x0,dev,ind)app_dir_deriv(@(x)orig_rhs(x,p),x0,dev,ind,hbif);
-    %          2nd order approx directional derivative in ith column of x0
-    %
-    %          dy = app_dir_deriv(f,x0,deviation,ind,h)
-    %          xd=x0;
-    %          xd(:,ind,:)=x0(:,ind,:)+h*deviation;
-    %          dy1=f(xd);
-    %          xd(:,ind,:)=x0(:,ind,:)-h*deviation;
-    %          dy2=f(xd);
-    %          dy=(dy1-dy2)/(2*h);
-
-    %% add partial derivatives of all terms
-    y1=0*y0; % Make a vector that is the right size but all zeros.
-    for i=1:size(x0,2)
-        y1=y1+dfx(x0,v(:,i),i);
-    end
-    dfom=app_dir_deriv(...
-        @(p)orig_rhs(x0,p),... % function to provide, devations are in par
-        p,...                  % parameter set
-        1,...                  % deviation = 1 * hbif
-        length(p),...          % select the parameter which is perturbed
-        hbif);                 % this is the size of the deviation.
-    y1=y1+dfom*rho;
-    y=[y0;y1];
-    
-    otherwise
-        
-        %% Now adding another omega!!
-        x0=x(1:dim,:); % the actual guess/soln vector
-        v=x(dim+1:end,:); % the nullvector
-        y0=orig_rhs(x0,p);
-        dfx=@(x0,dev,ind)app_dir_deriv(@(x)orig_rhs(x,p),x0,dev,ind,hbif);
-%          2nd order approx directional derivative in ith column of x0
-%
-%          dy = app_dir_deriv(f,x0,deviation,ind,h)
-%          xd=x0;
-%          xd(:,ind,:)=x0(:,ind,:)+h*deviation;
-%          dy1=f(xd);
-%          xd(:,ind,:)=x0(:,ind,:)-h*deviation;
-%          dy2=f(xd);
-%          dy=(dy1-dy2)/(2*h);
-
-        %% add partial derivatives of all terms
-        y1=0*y0; % Make a vector that is the right size but all zeros.
-        for i=1:size(x0,2)
-            y1=y1+dfx(x0,v(:,i),i);
-        end
-
-% HOW IT WORKS WITH 2 OMEGA, WITH THE LAST TWO PARAMS (... ,omega1, omega2)
-%
-%         % For omega 1
-%         dfom1=app_dir_deriv(...
-%             @(p)orig_rhs(x0,p),... % function to provide, devations are in par
-%             p,...                  % parameter set
-%             1,...                  % deviation = 1 * hbif
-%             length(p)-1,...        % select the parameter which is perturbed, omega
-%             hbif);                 % this is the size of the deviation.
-% 
-%         % For omega 2
-%         dfom2=app_dir_deriv(...
-%             @(p)orig_rhs(x0,p),... % function to provide, devations are in par
-%             p,...                  % parameter set
-%             1,...                  % deviation = 1 * hbif
-%             length(p),...          % select the parameter which is perturbed, omega
-%             hbif);                 % this is the size of the deviation.
-
-        dfom = cell(numel(rho));
-        % Create a dir deriv for each rho parameter we have. Organize them
-        % into a cell, so they can multiple by the parameter later on.
-        for i = 1:numel(rho)
-            TEMPdfom = app_dir_deriv(...
-            @(p)orig_rhs(x0,p),... % function to provide, devations are in par
-            p,...                  % parameter set
-            1,...                  % deviation = 1 * hbif
-            length(p)-1,...        % select the parameter which is perturbed, omega
-            hbif);                 % this is the size of the deviation.
-            
-            dfom{i} = TEMPdfom;
-        end
-        
-        % add each rho related deriv
-        for i = 1:numel(rho)
-            y1 = y1 + dfom{i}*rho(i);
-        end
-        y=[y0;y1];
-        
-end
-
-%}
 
 
 end
