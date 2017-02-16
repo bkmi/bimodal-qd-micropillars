1ce4a62e4f46b34294b0a009f5c6b5942dc7aff2
Ben M <benmiller1315@gmail.com>
2017-02-16 12:21:07 +0100

    Removed rot files, added path for ddebiftool_multirot git directory
    This is an update which puts all of the multirot files in their own git directory so they are easier to update/track

--- addpath_setup.m
+++ addpath_setup.m
@@ -1,11 +1,17 @@
 function [  ] = addpath_setup(  )
 %This function adds the relevant folders to matlab path.
 
+% Add multi_rot_sym directly from location on my own personal drive. If you
+% aren't me, and are hoping to use mult_rot then you will need to direct
+% this add path to the multirot directory in DDE-BIFTOOL.
+% addpath(strcat(present_working_directory,'/functions/rot'))
+addpath('/home/bkmiller/qd-micropillar-laser-project/ddebiftool_multirot')
+
 present_working_directory = pwd;
 addpath(present_working_directory)
 addpath(strcat(present_working_directory,'/functions/'))
 addpath(strcat(present_working_directory,'/functions/numerical_systems/'))
-addpath(strcat(present_working_directory,'/functions/rot'))
+
 addpath(strcat(present_working_directory,'/functions/bifcont_data_gen_tools'))
 addpath(strcat(present_working_directory,'/functions/data_analysis_tools'))
 addpath(strcat(present_working_directory,'/scripts/'))
