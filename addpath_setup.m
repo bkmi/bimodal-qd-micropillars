2fe80f4a50a430f3ab865c66dd5186c8afffa849
Ben M <benmiller1315@gmail.com>
2017-02-22 12:49:47 +0100

    Added submodule support for multirot

--- addpath_setup.m
+++ addpath_setup.m
@@ -1,12 +1,7 @@
 function [  ] = addpath_setup(  )
 %This function adds the relevant folders to matlab path.
 
-% Add multi_rot_sym directly from location on my own personal drive. If you
-% aren't me, and are hoping to use mult_rot then you will need to direct
-% this add path to the multirot directory in DDE-BIFTOOL.
-% addpath(strcat(present_working_directory,'/functions/rot'))
-addpath('/home/bkmiller/qd-micropillar-laser-project/ddebiftool_multirot')
-
+%% My Files
 present_working_directory = pwd;
 addpath(present_working_directory)
 addpath(strcat(present_working_directory,'/functions/'))
@@ -15,6 +10,9 @@ addpath(strcat(present_working_directory,'/functions/timeSeries_data_gen_tools')
 addpath(strcat(present_working_directory,'/functions/bifcont_data_gen_tools'))
 addpath(strcat(present_working_directory,'/functions/data_analysis_tools'))
 addpath(strcat(present_working_directory,'/scripts/'))
+
+%% DDE-BIFTOOL
+addpath(present_working_directory,'/ddebiftool_multirot/') % Multi_rot
 addpath('~/dde_biftool_v3.1.1/ddebiftool/'); 
 addpath('~/dde_biftool_v3.1.1/ddebiftool_extra_psol/');
 addpath('~/dde_biftool_v3.1.1/ddebiftool_utilities/');
