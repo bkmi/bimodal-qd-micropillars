aea40450b234028a5120cac58e6587a7332b619d
Ben M <benmiller1315@gmail.com>
2017-02-24 14:25:23 +0100

    Now generates an array of input-output curves

--- scripts/final_intensity_vs_current.m
+++ scripts/final_intensity_vs_current.m
@@ -22,151 +22,104 @@ Jarray = linspace(...
 %% Turn On Solver
 timeTotal = 0;
 
-for i = 1:numPoints
-    tic; % Start timer
-    
-    param = setup_params_nonDim_CnstCplRatio(...
-        'save',0, ...
-        'J', Jarray(i), ...
-        'feed_ampli',0, ...
-        'feed_ampliMatrix', [0, 0; 0, 0], ...
-        'feed_phase',0, ...
-        'clear',0,...
-        'populate_wrkspc', 0);
-    dde23_soln = solver([1e-9;0;1e-9;0;0;0], [0,5], param, 'quiet',1);
-    % 'dde23_options',ddeset('RelTol',10^-8,'OutputFcn', @odeplot)
-    
-    solnTurnOn(i).timeSeries = dde23_soln;
-    solnTurnOn(i).J = param.J.value;
-    solnTurnOn(i).param = param;
-    solnTurnOn(i).calcTime = toc;
-    timeTotal = timeTotal + toc;
+fbAmp = [0, 0.1,0.5,0.8];
+
+for j = fbAmp
+
+    for i = 1:numPoints
+        tic; % Start timer
+
+        param = setup_params_nonDim_CnstCplRatio(...
+            'save',0, ...
+            'J', Jarray(i), ...
+            'feed_ampli',j, ...
+            'feed_ampliMatrix', [1, 0; 0, 1], ...
+            'feed_phase',0, ...
+            'clear',0,...
+            'populate_wrkspc', 0);
+        dde23_soln = solver([1e-9;0;1e-9;0;0;0], [0,9], param, 'quiet',1);
+        % 'dde23_options',ddeset('RelTol',10^-8,'OutputFcn', @odeplot)
+
+        solnTurnOn(i).timeSeries = dde23_soln;
+        solnTurnOn(i).J = param.J.value;
+        solnTurnOn(i).param = param;
+        solnTurnOn(i).calcTime = toc;
+        timeTotal = timeTotal + toc;
+    end
+
+    disp('It took')
+    disp(timeTotal)
+
+    % Plot final intensity versus current amplitude
+
+    strongFinalIntensity = zeros(numPoints,1);
+    weakFinalIntensity = zeros(numPoints,1);
+    for i = 1:numPoints
+        strongFinalIntensity(i) = norm( ...
+            [solnTurnOn(i).timeSeries.y(1,end),...
+            solnTurnOn(i).timeSeries.y(2,end)] );
+        weakFinalIntensity(i) = norm( ...
+            [solnTurnOn(i).timeSeries.y(3,end),...
+            solnTurnOn(i).timeSeries.y(4,end)] );
+    end
+
+    fig1 = figure;
+    % strong red
+    semilogy(Jarray,strongFinalIntensity,'r')
+    hold on
+    % weak blue
+    semilogy(Jarray,weakFinalIntensity,'b')
+    title(['Turn on: Final intensity vs current amplitude, k =',num2str(j)])
+    hold off
 end
-
-disp('It took')
-disp(timeTotal)
-
-%% Plot final intensity versus current amplitude
-
-strongFinalIntensity = zeros(numPoints,1);
-weakFinalIntensity = zeros(numPoints,1);
-for i = 1:numPoints
-    strongFinalIntensity(i) = norm( ...
-        [solnTurnOn(i).timeSeries.y(1,end),...
-        solnTurnOn(i).timeSeries.y(2,end)] );
-    weakFinalIntensity(i) = norm( ...
-        [solnTurnOn(i).timeSeries.y(3,end),...
-        solnTurnOn(i).timeSeries.y(4,end)] );
-end
-
-fig1 = figure;
-% strong red
-semilogy(Jarray,strongFinalIntensity,'r')
-hold on
-% weak blue
-semilogy(Jarray,weakFinalIntensity,'b')
-title('Turn on final intensity vs current amplitude')
-hold off
-
-
-% %% Old Sweep Method
-% 
-% timeTotalSweep = 0;
-% 
-% solnSweep = repmat(...
-%     struct(...
-%     'timeSeries',struct, ...
-%     'J',0,...
-%     'params',struct, ...
-%     'calcTime',0),numPoints);
-% 
-% for i = 1
-%     tic; % Start timer
-% 
-%     setup_params_nonDim_CnstCplRatio(...
-%         'save',0, ...
-%         'J', Jarray(i), ...
-%         'feed_ampli',0, ...
-%         'feed_ampliMatrix', [0, 0; 0, 0], ...
-%         'feed_phase',0, ...
-%         'clear',0);
-%     dde23_soln = solver([1e-9;0;1e-9;0;0;0], [0,10], param, ...
-%         master_options, 'quiet', 1);
-%     % 'dde23_options',ddeset('RelTol',10^-8,'OutputFcn', @odeplot)
-% 
-%     solnSweep(i).timeSeries = dde23_soln;
-%     solnSweep(i).J = param.J.value;
-%     solnSweep(i).param = param;
-%     solnSweep(i).calcTime = toc;
-%     timeTotal = timeTotal + toc;
-% end
-% 
-% for i = 2:numPoints
-%     tic; % Start timer
-%     
-%     setup_params_nonDim_CnstCplRatio(...
-%         'save',0, ...
-%         'J', Jarray(i), ...
-%         'feed_ampli',0, ...
-%         'feed_ampliMatrix', [0, 0; 0, 0], ...
-%         'feed_phase',0, ...
-%         'clear',0);
-%     dde23_soln = solver(solnSweep(i-1).timeSeries, ...
-%         [10*(i-1),10*(i-1)+10], param, master_options, 'quiet', 1);
-%     % 'dde23_options',ddeset('RelTol',10^-8,'OutputFcn', @odeplot)
-%     
-%     solnSweep(i).timeSeries = dde23_soln;
-%     solnSweep(i).J = param.J.value;
-%     solnSweep(i).param = param;
-%     solnSweep(i).calcTime = toc;
-%     timeTotal = timeTotal + toc;
-% end
-% 
-% disp('It took')
-% disp(timeTotal)
 
 
 %% Sweep Solver
 
-param = setup_params_nonDim_CnstCplRatio(...
-    'save',0, ...
-    'J', Jarray(1), ...
-    'feed_ampli',0, ...
-    'feed_ampliMatrix', [0, 0; 0, 0], ...
-    'feed_phase',0, ...
-    'populate_wrkspc', 0, ...
-    'clear',0);
 
-solnSweep = sweeper(param.J.index, [Jmin, Jmax], param, 'plot',1);
+fbAmp = [0, 0.1,0.5,0.8];
 
-timeTotal = 0;
-for i = 1:numel(solnSweep)
-    timeTotal = timeTotal + solnSweep(i).calcTime;
+for j = fbAmp
+
+    param = setup_params_nonDim_CnstCplRatio(...
+        'save',0, ...
+        'J', Jarray(1), ...
+        'feed_ampli',j, ...
+        'feed_ampliMatrix', [1, 0; 0, 1], ...
+        'feed_phase',0, ...
+        'populate_wrkspc', 0, ...
+        'clear',0);
+
+    solnSweep = sweeper(param.J.index, [Jmin, Jmax], param, 'plot',1);
+
+    timeTotal = 0;
+    for i = 1:numel(solnSweep)
+        timeTotal = timeTotal + solnSweep(i).calcTime;
+    end
+
+    disp('It took')
+    disp(timeTotal)
+
+    % Plot final intensity versus current amplitude
+
+    strongFinalIntensitySweep = zeros(numPoints,1);
+    weakFinalIntensitySweep = zeros(numPoints,1);
+    for i = 1:numPoints
+        strongFinalIntensitySweep(i) = norm( ...
+            [solnSweep(i).timeSeries.y(1,end),...
+            solnSweep(i).timeSeries.y(2,end)] );
+        weakFinalIntensitySweep(i) = norm( ...
+            [solnSweep(i).timeSeries.y(3,end),...
+            solnSweep(i).timeSeries.y(4,end)] );
+    end
+
+
+    fig2 = figure;
+    % strong red
+    semilogy(Jarray,strongFinalIntensitySweep,'r')
+    hold on
+    % weak blue
+    semilogy(Jarray,weakFinalIntensitySweep,'b')
+    title(['Sweep: Final intensity vs Current amplitude, k =',num2str(j)])
+    hold off
 end
-
-disp('It took')
-disp(timeTotal)
-
-%% Plot final intensity versus current amplitude
-
-strongFinalIntensitySweep = zeros(numPoints,1);
-weakFinalIntensitySweep = zeros(numPoints,1);
-for i = 1:numPoints
-    strongFinalIntensitySweep(i) = norm( ...
-        [solnSweep(i).timeSeries.y(1,end),...
-        solnSweep(i).timeSeries.y(2,end)] );
-    weakFinalIntensitySweep(i) = norm( ...
-        [solnSweep(i).timeSeries.y(3,end),...
-        solnSweep(i).timeSeries.y(4,end)] );
-end
-
-
-fig2 = figure;
-% strong red
-semilogy(Jarray,strongFinalIntensitySweep,'r')
-hold on
-% weak blue
-semilogy(Jarray,weakFinalIntensitySweep,'b')
-title('Final intensity vs Current amplitude, "Sweep"')
-hold off
-
