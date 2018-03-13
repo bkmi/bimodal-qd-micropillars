e9a909bb38c23b3eb11fb4c70f84f8192b9975bd
Ben M <benmiller1315@gmail.com>
2018-03-13 16:05:20 +0100

    made unix compat

--- data_directory.m
+++ data_directory.m
@@ -1,9 +1,17 @@
 function [ directory ] = data_directory(  )
 %Returns string of main directory.
-filepath = mfilename('fullpath');
-parts = strsplit(filepath, '\');
-part = parts(1:end-1);
-directory = strjoin(part, '\');
-directory = strcat(directory, '\', 'output_data\');
+if ispc
+    filepath = mfilename('fullpath');
+    parts = strsplit(filepath, '\');
+    part = parts(1:end-1);
+    directory = strjoin(part, '\');
+    directory = strcat(directory, '\', 'output_data\');
+else
+    filepath = mfilename('fullpath');
+    parts = strsplit(filepath, '/');
+    part = parts(1:end-1);
+    directory = strjoin(part, '/');
+    directory = strcat(directory, '/', 'output_data\');
+end
 end
 
--- main_directory.m
+++ main_directory.m
@@ -1,9 +1,17 @@
 function [ directory ] = main_directory(  )
 %Returns string of main directory.
-filepath = mfilename('fullpath');
-parts = strsplit(filepath, '\');
-part = parts(1:end-1);
-directory = strjoin(part, '\');
-directory = strcat(directory, '\');
+if ispc
+    filepath = mfilename('fullpath');
+    parts = strsplit(filepath, '\');
+    part = parts(1:end-1);
+    directory = strjoin(part, '\');
+    directory = strcat(directory, '\');
+else
+    filepath = mfilename('fullpath');
+    parts = strsplit(filepath, '/');
+    part = parts(1:end-1);
+    directory = strjoin(part, '/');
+    directory = strcat(directory, '/');
+end
 end
 
