1e2ef9990cdd2885808d2a0aa6e2e94453884db3
Ben M <benmiller1315@gmail.com>
2018-03-26 14:21:09 +0200

    recalced phases for better plotting

--- specific_bifurcations/pretty_phases.m
+++ specific_bifurcations/pretty_phases.m
@@ -20,28 +20,121 @@ if 0
         'add_2_gcf', 1)
         pause(1)
     end
-    
-    save(strcat(datadir, 'phase_collection2'), 'folds2')
 end
 
 if exist('folds2')
     folds = folds2;
 end
 
-%% extend phase
-ok = bcont(funcs, phases{7}, 50, 50);
-ok = branch_stability(funcs, ok);
-figure
-plot_branch3( ok, ...
-        param, ...
-        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
-        'nunst_color', ok.nunst, ...
-        'add_2_gcf', 1)
+if 0
+    save(strcat(data_directory(), 'specific_bifurcations/folds'), ...
+        'foldfuncs', 'folds', 'param', 'st_bif', 'opt_inputs')
+end
 
-phases{7} = ok;
+if 0
+    % extend phase
+    ok = bcont(funcs, phases{7}, 25, 0);
+    ok = branch_stability(funcs, ok);
+    figure
+    plot_branch3( ok, ...
+            param, ...
+            'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+            'nunst_color', ok.nunst, ...
+            'add_2_gcf', 1)
+
+    phases{7} = ok;
+
+    % move 7 over by 2pi
+    ph = phases{7};
+
+    for i = 1:numel(ph.point)
+        ph.point(i).parameter(param.feed_phase.index) = ph.point(i).parameter(param.feed_phase.index) - 2*pi;
+    end
+
+    figure
+    for i = 1:numel(phases)
+        if i == 7
+            plot_branch3( ph, ...
+            param, ...
+            'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+            'nunst_color', ph.nunst, ...
+            'add_2_gcf', 1)
+        else
+            plot_branch3( phases{i}, ...
+                param, ...
+                'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+                'nunst_color', phases{i}.nunst, ...
+                'add_2_gcf', 1)
+        end
+    end
+
+    phases{7} = ph;
+end
+
+%% extend other than 7
+for i = 1:numel(phases)
+    if i ~= 7
+        phases{i}.parameter.min_bound = [param.feed_phase.index -18];
+        phases{i}.parameter.max_bound = [param.feed_phase.index 18];
+        phases{i}.parameter.max_step = [param.feed_phase.index, pi/40, ... 
+                                        param.omega1.index, 0.05];
+        phases{i} = bcont(funcs, phases{i}, 1000, 1000);
+        phases{i} = branch_stability(funcs, phases{i});
+    end
+end
+
+%% remake, other than 7
+rephases = phases;
+
+for i = 1:numel(rephases)
+    if i == 6
+        rephases{i}.point(end-4:end) = [];
+        rephases{i}.parameter.min_bound = [param.feed_phase.index -18];
+        rephases{i}.parameter.max_bound = [param.feed_phase.index 18];
+        rephases{i}.parameter.max_step = [param.feed_phase.index, pi/40, ... 
+                                        param.omega1.index, 0.05];
+        rephases{i} = bcont(funcs, rephases{i}, 2000, 2000);
+        rephases{i} = branch_stability(funcs, rephases{i});
+    elseif i == 7
+        rephases{i}.point(4:end) = [];
+        rephases{i}.parameter.min_bound = [param.feed_phase.index -18];
+        rephases{i}.parameter.max_bound = [param.feed_phase.index 18];
+        rephases{i}.parameter.max_step = [param.feed_phase.index, pi/40, ... 
+                                        param.omega1.index, 0.05];
+        rephases{i} = bcont(funcs, rephases{i}, 200, 200);
+        rephases{i} = branch_stability(funcs, rephases{i});
+    elseif i == 9 || i == 10
+        rephases{i}.point(4:end) = [];
+        rephases{i}.parameter.min_bound = [param.feed_phase.index -18];
+        rephases{i}.parameter.max_bound = [param.feed_phase.index 18];
+        rephases{i}.parameter.max_step = [param.feed_phase.index, pi/40, ... 
+                                        param.omega1.index, 0.05];
+        rephases{i} = bcont(funcs, rephases{i}, 9000, 9000);
+        rephases{i} = branch_stability(funcs, rephases{i});
+    elseif i ~= 7
+        rephases{i}.point(4:end) = [];
+        rephases{i}.parameter.min_bound = [param.feed_phase.index -18];
+        rephases{i}.parameter.max_bound = [param.feed_phase.index 18];
+        rephases{i}.parameter.max_step = [param.feed_phase.index, pi/40, ... 
+                                        param.omega1.index, 0.05];
+        rephases{i} = bcont(funcs, rephases{i}, 5000, 5000);
+        rephases{i} = branch_stability(funcs, rephases{i});
+    end
+end
+
+%% save?
+if 0
+    save(strcat(data_directory(), 'specific_bifurcations/phases'), ...
+        'funcs', 'phases', 'param', 'st_amp', 'st_phase', 'opt_inputs')
+end
+
+if 0
+    save(strcat(data_directory(), 'specific_bifurcations/rephases'), ...
+    'funcs', 'rephases', 'param', 'st_amp', 'st_phase', 'opt_inputs')
+end
 
 %% save just the collection
-if 1
+if 0
     % save location
     datadir = strcat(data_directory(), 'specific_bifurcations/');
     while isdir(datadir) == 0
@@ -49,8 +142,8 @@ if 1
     end
     
     save(strcat(datadir, 'pretty_phase'), ...
-        'funcs', 'foldfuncs', 'phases', 'folds', 'param', ...
-        'st_amp','st_phase', 'st_bif', 'opt_inputs')
+        'funcs', 'phases', 'param', ...
+        'st_amp','st_phase', 'opt_inputs')
 end
 
     
@@ -67,10 +160,67 @@ end
 
 %% plot
 figure
-for i = 1:numel(phases)
+for i = 10 % numel(phases)
     plot_branch3( phases{i}, ...
         param, ...
         'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
         'nunst_color', phases{i}.nunst, ...
+        'add_2_gcf', 1)
+%     pause(0.5)
+end
+
+%% plot rephaes
+figure
+for i = 1:numel(rephases)
+    plot_branch3( rephases{i}, ...
+        param, ...
+        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+        'nunst_color', rephases{i}.nunst, ...
+        'add_2_gcf', 1)
+end
+
+%% plot each
+figure
+for i = 1:3
+    plot_branch3( rephases{i}, ...
+        param, ...
+        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+        'nunst_color', rephases{i}.nunst, ...
+        'add_2_gcf', 1)
+end
+
+figure
+for i = 4:6
+    plot_branch3( rephases{i}, ...
+        param, ...
+        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+        'nunst_color', rephases{i}.nunst, ...
+        'add_2_gcf', 1)
+end
+
+figure
+for i = 7
+    plot_branch3( rephases{i}, ...
+        param, ...
+        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+        'nunst_color', rephases{i}.nunst, ...
+        'add_2_gcf', 1)
+end
+
+figure
+for i = 8:9
+    plot_branch3( rephases{i}, ...
+        param, ...
+        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+        'nunst_color', rephases{i}.nunst, ...
+        'add_2_gcf', 1)
+end
+
+figure
+for i = 10
+    plot_branch3( rephases{i}, ...
+        param, ...
+        'axes_indParam', {param.feed_phase.index, param.feed_ampli.index, 'x1'}, ...
+        'nunst_color', rephases{i}.nunst, ...
         'add_2_gcf', 1)
 end
